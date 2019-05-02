7e91d70b2674779f5d15418bca5dd647a1ed5a95
Edinson Nuñez More <nmedinson@gmail.com>
2019-05-02 16:21:49 -0500

    Updates

---
+++ precache-manifest.6b99d7c3d28bc72f6ab890a737e86672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6ec622c6bd3c238b82a54a7b457353b",
+    "url": "/EdinsonNM/ABtesting.git/index.html"
+  },
+  {
+    "revision": "0e62a4fb901d6dd98d60",
+    "url": "/EdinsonNM/ABtesting.git/static/css/main.8dc35571.chunk.css"
+  },
+  {
+    "revision": "75e2acee17a447f1c047",
+    "url": "/EdinsonNM/ABtesting.git/static/js/2.63e1e3f4.chunk.js"
+  },
+  {
+    "revision": "0e62a4fb901d6dd98d60",
+    "url": "/EdinsonNM/ABtesting.git/static/js/main.64a38664.chunk.js"
+  },
+  {
+    "revision": "08481d7fedf7eb8a2333",
+    "url": "/EdinsonNM/ABtesting.git/static/js/runtime~main.9e92f29e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EdinsonNM/ABtesting.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
