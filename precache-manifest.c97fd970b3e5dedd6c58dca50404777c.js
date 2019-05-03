d38f5d9ebe43253ccdd64c437aa0cf80705cf5d7
Edinson Nuñez More <nmedinson@gmail.com>
2019-05-03 09:39:14 -0500

    Updates

---
+++ precache-manifest.c97fd970b3e5dedd6c58dca50404777c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ccdbb964c5360dd5eeb20bc2328a2d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "780cd64de3fdfa8575bd",
+    "url": "/static/css/main.8dc35571.chunk.css"
+  },
+  {
+    "revision": "237cc7f31eb15e0dd624",
+    "url": "/static/js/2.a2f723e8.chunk.js"
+  },
+  {
+    "revision": "780cd64de3fdfa8575bd",
+    "url": "/static/js/main.84ce6c88.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
